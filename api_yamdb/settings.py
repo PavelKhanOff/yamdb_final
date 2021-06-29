57f137c3cac9acb5a97a3d123284590f17683369
PavelKhan <pavelkhanoff@gmail.com>
2021-06-30 00:54:36 +0600

    2nd try

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -1,6 +1,8 @@
 import os
 from datetime import timedelta
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
--- yamdb/views.py
+++ yamdb/views.py
@@ -129,8 +129,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
-        queryset = title.reviews.all()
-        return queryset
+        return title.reviews.all()
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
